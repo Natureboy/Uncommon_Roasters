ea7d6b1ceead6af0514df6a777c8b5d457450a93
Alex Suriano <suriano.alex@gmail.com>
2015-02-15 15:30:20 -0500

    Got Website button launching browser with Uncommon website

--- app/src/main/java/com/teamcoffee/coffeewizard/MainActivity.java
+++ app/src/main/java/com/teamcoffee/coffeewizard/MainActivity.java
@@ -1,10 +1,12 @@
 package com.teamcoffee.coffeewizard;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -36,5 +38,12 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void openWeb(View view){
+        String url = "https://uncommoncoffeeroasters.com/";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 }
