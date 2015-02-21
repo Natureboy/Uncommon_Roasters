852ee5cd5ebb25580b62d794bd52ae0ffd9c1b98
bkoning23 <koningb@mail.gvsu.edu>
2015-02-21 18:14:01 -0500

    Added Empty Dial In page that opens from the "Dial In" button on the main page.

--- app/src/main/java/com/teamcoffee/coffeewizard/dial_in.java
+++ app/src/main/java/com/teamcoffee/coffeewizard/dial_in.java
@@ -1,26 +1,26 @@
 package com.teamcoffee.coffeewizard;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends ActionBarActivity {
+
+public class dial_in extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
+        setContentView(R.layout.activity_dial_in);
+        Intent intent = getIntent();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_dial_in, menu);
         return true;
     }
 
@@ -38,18 +38,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void openWeb(View view){
-        String url = "https://uncommoncoffeeroasters.com/";
-        Intent i = new Intent(Intent.ACTION_VIEW);
-        i.setData(Uri.parse(url));
-        startActivity(i);
-    }
-
-    public void openDial_in(View view){
-        Intent i = new Intent(this, dial_in.class);
-        startActivity(i);
-    }
-
-
 }
