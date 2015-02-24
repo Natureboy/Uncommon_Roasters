9834ea6da44afba8da8f89c87740054b95054f0c
Alex Suriano <suriano.alex@gmail.com>
2015-02-23 19:30:24 -0500

    Added two methods to main activity to determine screen size and density.

--- app/src/main/java/com/teamcoffee/coffeewizard/MainActivity.java
+++ app/src/main/java/com/teamcoffee/coffeewizard/MainActivity.java
@@ -1,12 +1,16 @@
 package com.teamcoffee.coffeewizard;
 
+
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -14,7 +18,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        View con = this.findViewById(android.R.id.content);
+        String size= getSizeInfo(con);
+        String den= getDenInfo();
+
     }
 
     @Override
@@ -49,6 +56,61 @@ public class MainActivity extends ActionBarActivity {
     public void openDial_in(View view){
         Intent i = new Intent(this, dial_in.class);
         startActivity(i);
+    }
+
+    //Determine screen size
+    private static String getSizeInfo(View view) {
+        int screenLayout = view.getResources().getConfiguration().screenLayout;
+        screenLayout &= Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        switch(screenLayout){
+            case Configuration.SCREENLAYOUT_SIZE_XLARGE:
+                System.out.println("The size is xl");
+                return "xl";
+            case Configuration.SCREENLAYOUT_SIZE_LARGE:
+                System.out.println("The size is large");
+                return "large";
+            case Configuration.SCREENLAYOUT_SIZE_NORMAL:
+                System.out.println("The size is normal");
+                return "normal";
+            case Configuration.SCREENLAYOUT_SIZE_SMALL:
+                System.out.println("The size is small");
+                return "small";
+            default:
+                System.out.println("The size is unknown");
+                return "unknown";
+        }
+
+    }
+    //Determine screen density
+    private String getDenInfo() {
+        DisplayMetrics metrics= new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int density= metrics.densityDpi;
+
+        switch(density){
+            case DisplayMetrics.DENSITY_XXXHIGH:
+                System.out.println("The density is xxxhigh\n");
+                return "xxxhigh";
+            case DisplayMetrics.DENSITY_XXHIGH:
+                System.out.println("The density is xxhigh\n");
+                return "xxhigh";
+            case DisplayMetrics.DENSITY_XHIGH:
+                System.out.println("The density is xhigh\n");
+                return "xhigh";
+            case DisplayMetrics.DENSITY_HIGH:
+                System.out.println("The density is high\n");
+                return "high";
+            case DisplayMetrics.DENSITY_MEDIUM:
+                System.out.println("The density is med\n");
+                return "med";
+            case DisplayMetrics.DENSITY_LOW:
+                System.out.println("The density is low\n");
+                return "low";
+            default:
+                System.out.println("The density is unknown");
+                return "unknown";
+        }
     }
 
 
