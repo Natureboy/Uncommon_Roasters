ec6233677a77d5bfaa35a66037a9e0a8fa25a213
Alex Suriano <suriano.alex@gmail.com>
2015-03-09 20:38:33 -0400

    Got FAQ page functional. Have to add top banner and work on sizing

---
+++ app/src/main/java/com/teamcoffee/coffeewizard/FAQActivity.java
@@ -0,0 +1,127 @@
+package com.teamcoffee.coffeewizard;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
+
+public class FAQActivity extends Activity {
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.elv);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        if(expListView!=null) {
+           System.out.println("Success");
+            expListView.setAdapter(listAdapter);
+        }
+        else{
+            System.out.println("Failure");
+        }
+        // Listview Group click listener
+        expListView.setOnGroupClickListener(new OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                // Toast.makeText(getApplicationContext(),
+                // "Group Clicked " + listDataHeader.get(groupPosition),
+                // Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Expanded",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Collapsed",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+
+                Toast.makeText(
+                        getApplicationContext(),
+                        listDataHeader.get(groupPosition)
+                                + " : "
+                                + listDataChild.get(
+                                listDataHeader.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
+    }
+
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Grinding");
+        listDataHeader.add("Water Quality");
+        listDataHeader.add("Density");
+
+        // Adding child data
+        List<String> grind = new ArrayList<String>();
+        grind.add("You must grind the coffee before you brew");
+
+        List<String> waterQ = new ArrayList<String>();
+        waterQ.add("Don't use dirty water you fool");
+
+        List<String> density = new ArrayList<String>();
+         density.add("Mass/ volume");
+         density.add("this coffee is dense");
+
+        listDataChild.put(listDataHeader.get(0), grind); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), waterQ);
+        listDataChild.put(listDataHeader.get(2), density);
+    }
+}
