18e51c3559023cb7a5d358cb196e71c5cd27e300
bkoning23 <koningb@mail.gvsu.edu>
2015-03-14 23:52:08 -0400

    Updated Dial-In screen to not show screen elements when they do not apply to the brew. Added blank instructions to timer when the user is doing nothing in the brewing process.

--- app/src/main/java/com/teamcoffee/coffeewizard/DialActivity.java
+++ app/src/main/java/com/teamcoffee/coffeewizard/DialActivity.java
@@ -1,6 +1,8 @@
 package com.teamcoffee.coffeewizard;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -17,6 +19,7 @@ import android.widget.TextView;
 
 
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,8 +27,9 @@ public class DialActivity extends ActionBarActivity {
 
     private SeekBar water;
     private SeekBar density;
-    private TextView waterVolume, coffeeWeight, coffeeDensity;
+    private TextView waterVolume, coffeeWeight, coffeeDensity, coffeeDensityLabel;
     private Spinner spinner;
+    private AlertDialog.Builder builder1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,7 @@ public class DialActivity extends ActionBarActivity {
         waterVolume = (TextView)findViewById(R.id.waterVolumeNumber);
         coffeeDensity = (TextView) findViewById(R.id.coffeeDensityValue);
         waterVolume.setText(Integer.toString(water.getProgress()));
+        coffeeDensityLabel = (TextView) findViewById(R.id.selectDensityText);
         coffeeDensity.setText("Medium Density");
         waterVolume.setText("200");
 
@@ -123,30 +128,39 @@ public class DialActivity extends ActionBarActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if(spinner.getSelectedItem().toString().equals("V60")){
-                    setScreenElements(false, "12", 2, 0, "200");
+                    setScreenElements(false, "12", 2, 0, "200", View.VISIBLE);
                 }
                 else if(spinner.getSelectedItem().toString().equals("Press Pot")){
-                    setScreenElements(false, "15", 400, 200, "200");
+                    setScreenElements(false, "15", 400, 200, "200", View.GONE);
                 }
-
-
-
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
 
+        builder1 = new AlertDialog.Builder(this);
+        builder1.setCancelable(true);
+        builder1.setPositiveButton("Got it!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+
     }
 
-    private void setScreenElements(boolean weightEnabled, String weightValue, int waterMax, int waterProgress, String waterValue){
+    private void setScreenElements(boolean weightEnabled, String weightValue, int waterMax, int waterProgress, String waterValue,
+                                   int densityVisibility){
         coffeeWeight.setEnabled(weightEnabled);
         water.setMax(waterMax);
         water.setProgress(waterProgress);
         coffeeWeight.setText(weightValue);
         waterVolume.setText(waterValue);
+        coffeeDensity.setVisibility(densityVisibility);
+        density.setVisibility(densityVisibility);
+        coffeeDensityLabel.setVisibility(densityVisibility);
     }
 
     @Override
@@ -176,13 +190,39 @@ public class DialActivity extends ActionBarActivity {
         String weightLevel;
         Recipe result;
         int time;
-        String brewer, densityLevel, time_select_query, event_select_query;
+        String brewer, densityLevel, time_select_query, event_select_query, brewer_list_query;
+
+
+        DatabaseHelper dbHelper = new DatabaseHelper(DialActivity.this);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        brewer_list_query = "SELECT DISTINCT " + DatabaseContract.TableOne.COLUMN1_NAME + " FROM " + DatabaseContract.TableOne.TABLE_NAME;
+
+        Cursor c = db.rawQuery(brewer_list_query, null);
+
+        String[] brewerList =  new String[c.getCount()];
+
+        int i = 0;
+
+        while(c.moveToNext()){
+            brewerList[i] = c.getString(c.getColumnIndexOrThrow("machine"));
+            i++;
+        }
+
+        brewer = spinner.getSelectedItem().toString();
+
+        if(!Arrays.asList(brewerList).contains(brewer)){
+            builder1.setMessage("Select a brewer from the dropdown list.");
+            AlertDialog alert1 = builder1.create();
+            alert1.show();
+            return;
+        }
 
 
         waterLevel = (waterVolume.getText().toString());
         densityLevel = coffeeDensity.getText().toString();
         weightLevel = (coffeeWeight.getText().toString());
-        brewer = spinner.getSelectedItem().toString();
+
 
         switch (densityLevel) {
             case "Low Density":
@@ -200,15 +240,16 @@ public class DialActivity extends ActionBarActivity {
         time_select_query = DatabaseContract.TableOne.createSelect(brewer, waterLevel, weightLevel, densityLevel);
         event_select_query = DatabaseContract.TableTwo.createSelect(brewer, waterLevel, densityLevel);
 
+
+
         if (brewer.equals("Press Pot")){
             time_select_query = DatabaseContract.TableOne.createSelect(brewer, "0", "0", "n/a");
             event_select_query = DatabaseContract.TableTwo.createSelect(brewer, "0", "n/a");
         }
 
-        DatabaseHelper dbHelper = new DatabaseHelper(DialActivity.this);
-        SQLiteDatabase db = dbHelper.getReadableDatabase();
 
-        Cursor c = db.rawQuery(time_select_query, null);
+
+        c = db.rawQuery(time_select_query, null);
         if(c.moveToFirst()){
             time = c.getInt(c.getColumnIndex("brewTime"));
         }
@@ -239,11 +280,11 @@ public class DialActivity extends ActionBarActivity {
             timerEvents.put(0, "Pour to " + waterLevel + "g of Water");
         }
 
-        Intent i = new Intent(this, CountdownActivity.class);
-        i.putExtra("time", Integer.toString(time));
-        i.putExtra("events", timerEvents);
+        Intent intent = new Intent(this, CountdownActivity.class);
+        intent.putExtra("time", Integer.toString(time));
+        intent.putExtra("events", timerEvents);
 
-        startActivity(i);
+        startActivity(intent);
     }
 
 
