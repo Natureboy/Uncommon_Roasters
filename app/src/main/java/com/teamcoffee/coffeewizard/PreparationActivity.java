956ddc4e38dd3e79a455652c3da8f8d81b743927
bkoning23 <koningb@mail.gvsu.edu>
2015-03-16 20:11:53 -0400

    Added new "Intermediate" activity where more information about the brewing process can be conveyed to user about what they will need to successfully brew.

---
+++ app/src/main/java/com/teamcoffee/coffeewizard/PreparationActivity.java
@@ -0,0 +1,53 @@
+package com.teamcoffee.coffeewizard;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class PreparationActivity extends ActionBarActivity {
+
+    String weight, time;
+    TextView prepWeight, prepTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preparation);
+        Intent intent = getIntent();
+        weight = intent.getStringExtra("weight");
+        time = intent.getStringExtra("time");
+        prepWeight = (TextView) findViewById(R.id.prepWeight);
+        prepTime = (TextView) findViewById(R.id.prepTime);
+
+        prepTime.setText(time);
+        prepWeight.setText(weight);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_preparation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
